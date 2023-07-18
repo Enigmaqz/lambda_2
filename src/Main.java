88802a874d388bec47443af5d1f72447aa36bd5b
Boffka92 <boffka92@ya.ru>
2023-07-18 23:08:32 +0300

    Second part of exercise is done

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
 
-        Worker worker = new Worker(listener);
+
+        Worker worker = new Worker(listener, errorListener);
         worker.start();
     }
 }
--- src/OnTaskDoneListener.java
+++ src/OnTaskDoneListener.java
@@ -3,3 +3,5 @@ public interface OnTaskDoneListener {
     void onDone(String result);
 
 }
+
+
